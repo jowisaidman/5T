6fe4d3109df518c5b23ba3d851bf8db3ed3725a0
jowisaidmajn <joelsaidman1@gmail.com>
2020-03-31 15:13:15 -0300

    agrego pagina basica de usuario no loggeado

---
+++ 5-tenedores/app/screens/Account/UserGuest.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { StyleSheet, View, ScrollView, Text, Image } from 'react-native';
+import { Button } from 'react-native-elements';
+
+export default function UserGuest() {
+    return (
+        <ScrollView style={styles.viewBody} centerContent={true}>
+            <Image
+                source={require('../../../assets/img/user-guest.jpg')}
+                style={styles.image}
+                resizeMode='contain'
+            >
+
+            </Image>
+        <Text style={styles.title}>
+            Consulta tu perfil de 5T
+        </Text>
+        <Text style={styles.description}>
+            ¿Como describirias tu mejor restaurante? Busca y visualiza los mejores restaurantes de una forma sencilla. 
+            Vota cual te a gustado mas y comenta tu experiencia
+        </Text>
+        <View style={styles.viewBtn}>
+            <Button
+                buttonStyle={styles.btnStyle}
+                containerStyle={styles.btnContainer}
+                title='Ver tu perfil'
+                onPress={() => console.log('Presiono boton')}
+            />
+        </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    viewBody: {
+        marginLeft:30,
+        marginRight:30
+    },
+    image: {
+        width: '100%',
+        height: 300,
+        marginBottom: 40
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 19,
+        marginBottom: 10,
+        textAlign: 'center'
+    },
+    description: {
+        textAlign: 'center',
+        marginBottom: 20
+    },
+    viewBtn: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    btnStyle: {
+        backgroundColor: '#00a680',
+    },
+    btnContainer: {
+        width: '70%',
+    }
+})
