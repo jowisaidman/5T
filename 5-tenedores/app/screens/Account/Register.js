ceeae487bfef1dfb8cf2b1ce02f1299b2c153c3c
jowisaidmajn <joelsaidman1@gmail.com>
2020-04-01 20:54:56 -0300

    agrego estructura para registrar usuarios

--- 5-tenedores/app/screens/Account/Register.js
+++ 5-tenedores/app/screens/Account/Register.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { StyleSheet, View, Image, Text } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
 
 export default function Register() {
     return (
-        <View>
-            <Text>
-                Registro de usuarios....
-            </Text>
-        </View>
+        <KeyboardAwareScrollView>
+            <Image source={require('../../../assets/img/logo.png')}
+            style={styles.logo}
+            resizeMode='contain'/>
+            <View style={styles.viewForm}>
+                <Text>Form register....</Text>
+            </View>
+        </KeyboardAwareScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    logo: {
+        width: '100%',
+        height: 150,
+        marginTop: 20
+    },
+    viewForm: {
+        marginRight: 40,
+        marginLeft: 40
+    }
+})
